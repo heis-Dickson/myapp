56f2a161642cf2f9bcab26941690b215fa134d72
certifiedmayor6 <certifiedmayor6@gmail.com>
2025-08-11 21:26:53 +0000

    commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home_Page(),
+    );  
+  }
+}
+
+class Home_Page extends StatefulWidget {
+  const Home_Page({super.key});
+
+  @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
