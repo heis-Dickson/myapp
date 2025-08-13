82ffe0d6f765c97bfc672f94b5b3a912cb8022bd
certifiedmayor6 <certifiedmayor6@gmail.com>
2025-08-13 20:58:24 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,6 +25,19 @@ class Home_Page extends StatefulWidget {
   State<Home_Page> createState() => _Home_PageState();
 }
 
+const items = [1,2,3,4,5,6,7,8,9, 10];
+
+List<Widget> containers = items.map((item){
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      color: Colors.blue,
+      height: 200,
+      child: Center(child: Text('$item'))
+    ),
+  );
+}).toList();
+
 class _Home_PageState extends State<Home_Page> {
   @override
   Widget build(BuildContext context) {
@@ -38,18 +53,12 @@ class _Home_PageState extends State<Home_Page> {
         ),
       ),
       drawer: Drawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(color: Colors.blue, height: 200),
-            Divider(),
-            Container(color: Colors.blue, height: 200),
-            Divider(),
-            Container(color: Colors.blue, height: 200),
-            Divider(),
-            Container(color: Colors.blue, height: 200),
-          ],
-        ),
+      body: ListView.builder(
+      itemCount: containers.length,
+      itemBuilder: (context, index) {
+        return containers[index];
+      },
+      
       ),
       bottomNavigationBar: const BottomAppBar(color: Colors.red),
     );
