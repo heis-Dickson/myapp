0a8c5d03c2b5ecd1195789b2038ec7e621d4cceb
certifiedmayor6 <certifiedmayor6@gmail.com>
2025-08-14 20:03:54 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,17 @@ class _Home_PageState extends State<Home_Page> {
       },
       
       ),
-      bottomNavigationBar: const BottomAppBar(color: Colors.red),
+      bottomNavigationBar: const BottomAppBar(
+      color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(icon: Icon(Icons.home, size: 34), onPressed: null),
+          IconButton(icon: Icon(Icons.settings, size: 34), onPressed: null),
+          IconButton(icon: Icon(Icons.search, size: 34), onPressed: null),
+      ],
+      ),
+    ),
     );
   }
 }
